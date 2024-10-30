8679253ae7510a33ceec7ad9ea5c7f284561db7b
Ravipati989 <118012337+Ravipati989@users.noreply.github.com>
2024-10-30 20:09:07 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   features {}
 
-  subscription_id = "76589609-f356-45d6-9343-cacd37492e24"
+  subscription_id = "08e9cbbc-a29e-488d-8477-b22ab74e9325"
 }
 
 # Define the resource group
