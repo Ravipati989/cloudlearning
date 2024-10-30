293eb3beaba0fabe4f590b7540d1c4d234bf1762
Sa1ndeep <sandeepchowdary343@gmail.com>
2024-10-30 20:16:22 +0000

    deleted unwanted files

--- sqlserver.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_mssql_server" "cloudlearningsqlserver1" {
-  name                         = "cloudlearningsqlserver1"
-  resource_group_name          = azurerm_resource_group.Cloudlearning.name
-  location                     = azurerm_resource_group.Cloudlearning.location
-  version                      = "12.0"
-  administrator_login          = "sqladminuser"
-  administrator_login_password = "P@ssw0rd123!"  # Use strong password or pass via variable/secret
-}
