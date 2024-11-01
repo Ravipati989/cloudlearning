c8baf50e32a15407b24a1db4ea8e2c9631b12cb8
Ravipati989 <118012337+Ravipati989@users.noreply.github.com>
2024-11-01 14:05:57 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,16 +4,9 @@ provider "azurerm" {
     subscription_id = "76589609-f356-45d6-9343-cacd37492e24"
 }
 
-provider "azurerm" {
-  alias           = "sub2"  # Unique alias for the second subscription
-  features {}
-  subscription_id = "08e9cbbc-a29e-488d-8477-b22ab74e9325"
-  
-}
 
 # Define the resource group
 resource "azurerm_resource_group" "Cloudlearning" {
   name     = "Cloudlearning"
   location = "UKSouth"
-  provider = azurerm
 }
