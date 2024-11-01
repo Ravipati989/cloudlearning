5569ac4e4450d3e02ab019571cba83de747272b3
Ravipati989 <118012337+Ravipati989@users.noreply.github.com>
2024-11-01 13:56:31 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
 provider "azurerm" {
   features {}
 
-  Subscription = "76589609-f356-45d6-9343-cacd37492e24"
+    subscription_id = "76589609-f356-45d6-9343-cacd37492e24"
+}
+
+provider "azurerm" {
+  alias           = "sub2"  # Unique alias for the second subscription
+  features {}
+  subscription_id = "08e9cbbc-a29e-488d-8477-b22ab74e9325"
+  
 }
 
 # Define the resource group
